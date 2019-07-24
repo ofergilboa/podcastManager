cd770b2bc94dee2bf097b6ee2d5617a9ce85aaff
morales85 <mor4les85@gmail.com>
2019-07-24 11:41:15 +0300

    only 1 image in episodes

--- dist/main.js
+++ dist/main.js
@@ -21,3 +21,13 @@ const getPods = async function () {
         console.log(podID)
         renderer.render2(podManager.getEpisodesData(podID))
     })
+
+    Handlebars.registerHelper('each_upto', function(ary, max, options) {
+        if(!ary || ary.length == 0)
+            return options.inverse(this);
+    
+        var result = [ ];
+        for(var i = 0; i < max && i < ary.length; ++i)
+            result.push(options.fn(ary[i]));
+        return result.join('');
+    });
--- dist/renderer.js
+++ dist/renderer.js
@@ -27,4 +27,5 @@ class Renderer {
         $('.episodes').append(someHTML)
     }
 
+    
 }
